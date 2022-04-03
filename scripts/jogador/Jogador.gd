8d4b783591dc29abdc4a8b96f390c6957f21d82d
Guilherme <gui.crb0@gmail.com>
2022-04-03 16:29:07 -0300

    animacao nos eixos transversais

--- scripts/jogador/Jogador.gd
+++ scripts/jogador/Jogador.gd
@@ -14,18 +14,40 @@ func _ready():
 	mudarAnimacao("paradoBaixo")
 	
 func rodar():
+	
 	#fisica
 	if apertouBotao("baixo"):
 		sentido = "baixo"
 		direcao.y = velocidade
+	elif apertouBotao("cima"):
+		sentido = "cima"
+		direcao.y = -velocidade
 	else:
 		direcao.y = 0
-	
+		
+	if apertouBotao("esquerda"):
+		sentido = "esquerda"
+		direcao.x = -velocidade
+	elif apertouBotao("direita"):
+		sentido = "direita"
+		direcao.x = velocidade
+	else:
+		direcao.x = 0
+		
 	mover()
 	
 	#animacao
 	if apertouBotao("baixo"):
 		mudarAnimacao("andandoBaixo")
+	elif apertouBotao("cima"):
+		mudarAnimacao("andandoCima")
+	
+	if apertouBotao("esquerda"):
+		mudarAnimacao("andandoHorizontal")
+		inverterSentidoSprite(true)
+	elif apertouBotao("direita"):
+		mudarAnimacao("andandoHorizontal")
+		inverterSentidoSprite(false)
 
 	if estaParado():
 		if sentido == "baixo":
@@ -45,3 +67,12 @@ func apertouBotao(botao):
 	if botao == "baixo":
 		#impedir que o usuario aperte dois botoes que são exclusivos
 		return Input.is_action_pressed("baixo") and not Input.is_action_pressed("cima")
+	elif botao == "cima":
+		return Input.is_action_pressed("cima") and not Input.is_action_pressed("baixo")
+	elif botao == "esquerda":
+		return Input.is_action_pressed("esquerda") and not Input.is_action_pressed("direita")
+	elif botao == "direita":
+		return Input.is_action_pressed("direita") and not Input.is_action_pressed("esquerda")
+
+func inverterSentidoSprite(b):
+	$Sprite.flip_h = b
