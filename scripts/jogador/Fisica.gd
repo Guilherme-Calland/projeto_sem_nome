3f652e38e0f013d34a41849783032f8aa3e0a9b8
Guilherme <gui.crb0@gmail.com>
2022-04-04 13:03:38 -0300

    extract fisica node

---
+++ scripts/jogador/Fisica.gd
@@ -0,0 +1,41 @@
+extends Node
+
+# velocidade do boneco, export para podermos modificar enquanto jogamos
+export var velocidade = 75
+# vetor de direcao
+var direcao = Vector2(0,0)
+# para sabermos qual sentido ele esta olhando quando esta parado
+var sentido = "sul"
+#vetores
+var vetorIsometrico = Vector2(1, 0.5)
+var vetorSul = Vector2(0, vetorIsometrico.y)
+var vetorLeste = Vector2(vetorIsometrico.x, 0)
+var vetorNorte = Vector2(0, -vetorIsometrico.y)
+var vetorOeste = Vector2(-vetorIsometrico.x, 0)
+
+func rodar(atrito, input):
+	if input.apertouBotao("sul"):
+		sentido = "sul"
+		# clamp(valor, limite inferior, limite superior)
+		direcao.y = clamp(direcao.y + (vetorSul.y * atrito), vetorNorte.y * velocidade, vetorSul.y * velocidade)
+	elif input.apertouBotao("norte"):
+		sentido = "norte"
+		direcao.y = clamp(direcao.y + (vetorNorte.y * atrito), vetorNorte.y * velocidade, vetorSul.y * velocidade)
+	else:
+		if direcao.y > 0:
+			direcao.y = clamp(direcao.y + (vetorNorte.y * atrito), 0, direcao.y)
+		elif direcao.y < 0:
+			direcao.y = clamp(direcao.y + (vetorSul.y * atrito), direcao.y, 0)
+		
+	if input.apertouBotao("oeste"):
+		sentido = "horizontal"
+		direcao.x = clamp(direcao.x + (vetorOeste.x * atrito), vetorOeste.x * velocidade, vetorLeste.x * velocidade)
+	elif input.apertouBotao("leste"):
+		sentido = "horizontal"
+		direcao.x = clamp(direcao.x + (vetorLeste.x * atrito), vetorOeste.x * velocidade, vetorLeste.x * velocidade)
+	else:
+		if direcao.x > 0:
+			direcao.x = clamp(direcao.x + vetorOeste.x * atrito, 0, direcao.x)
+		elif direcao.x < 0:
+			direcao.x = clamp(direcao.x + vetorLeste.x * atrito, direcao.x, 0)
+		
--- scripts/jogador/Jogador.gd
+++ scripts/jogador/Jogador.gd
@@ -1,21 +1,5 @@
 extends KinematicBody2D
 
-# velocidade do boneco, export para podermos modificar enquanto jogamos
-export var velocidade = 75
-
-# fisica
-var vetorIsometrico = Vector2(1, 0.5)
-var vetorSul = Vector2(0, vetorIsometrico.y)
-var vetorLeste = Vector2(vetorIsometrico.x, 0)
-var vetorNorte = Vector2(0, -vetorIsometrico.y)
-var vetorOeste = Vector2(-vetorIsometrico.x,0)
-
-# vetor de direcao
-var direcao = Vector2(0,0)
-
-# para sabermos qual sentido ele esta olhando quando esta parado
-var sentido = "sul"
-
 # metodo que roda quando inicia essa cena
 func _ready():
 	mudarAnimacao("paradoSul")
@@ -23,31 +7,7 @@ func _ready():
 func rodar(atrito):
 	
 	#fisica
-	if $Input.apertouBotao("sul"):
-		sentido = "sul"
-		# clamp(valor, limite inferior, limite superior)
-		direcao.y = clamp(direcao.y + (vetorSul.y * atrito), vetorNorte.y * velocidade, vetorSul.y * velocidade)
-	elif $Input.apertouBotao("norte"):
-		sentido = "norte"
-		direcao.y = clamp(direcao.y + (vetorNorte.y * atrito), vetorNorte.y * velocidade, vetorSul.y * velocidade)
-	else:
-		if direcao.y > 0:
-			direcao.y = clamp(direcao.y + (vetorNorte.y * atrito), 0, direcao.y)
-		elif direcao.y < 0:
-			direcao.y = clamp(direcao.y + (vetorSul.y * atrito), direcao.y, 0)
-		
-	if $Input.apertouBotao("oeste"):
-		sentido = "horizontal"
-		direcao.x = clamp(direcao.x + (vetorOeste.x * atrito), vetorOeste.x * velocidade, vetorLeste.x * velocidade)
-	elif $Input.apertouBotao("leste"):
-		sentido = "horizontal"
-		direcao.x = clamp(direcao.x + (vetorLeste.x * atrito), vetorOeste.x * velocidade, vetorLeste.x * velocidade)
-	else:
-		if direcao.x > 0:
-			direcao.x = clamp(direcao.x + vetorOeste.x * atrito, 0, direcao.x)
-		elif direcao.x < 0:
-			direcao.x = clamp(direcao.x + vetorLeste.x * atrito, direcao.x, 0)
-		
+	$Fisica.rodar(atrito, $Input)
 	mover()
 	
 	#animacao
@@ -73,23 +33,20 @@ func rodar(atrito):
 			mudarAnimacao("andandoHorizontal")
 
 	if nenhumBotaoDirecaoApertado():
-		if sentido == "sul":
+		if $Fisica.sentido == "sul":
 			mudarAnimacao("paradoSul")
-		elif sentido == "diagonalSul":
+		elif $Fisica.sentido == "diagonalSul":
 			mudarAnimacao("paradoDiagonalSul")
-		elif sentido == "horizontal":
+		elif $Fisica.sentido == "horizontal":
 			mudarAnimacao("paradoHorizontal")
-		elif sentido == "diagonalNorte":
+		elif $Fisica.sentido == "diagonalNorte":
 			mudarAnimacao("paradoDiagonalNorte")
-		elif sentido == "norte":
+		elif $Fisica.sentido == "norte":
 			mudarAnimacao("paradoNorte")
 			
 func mover():
 	#metodo que precisa rodar para ele se mover a cada frame
-	move_and_slide(direcao, vetorNorte)
-	
-func estaParado():
-	return direcao == Vector2(0,0)
+	move_and_slide($Fisica.direcao, $Fisica.vetorNorte)
 
 func nenhumBotaoDirecaoApertado():
 	return not ($Input.apertouBotao("norte") || $Input.apertouBotao("sul") || $Input.apertouBotao("leste") || $Input.apertouBotao("oeste"))
