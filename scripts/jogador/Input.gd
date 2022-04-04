140d684ccc386c3071e6b72a9687ebc119d31144
Guilherme <gui.crb0@gmail.com>
2022-04-04 12:54:23 -0300

    extract input node

---
+++ scripts/jogador/Input.gd
@@ -0,0 +1,14 @@
+extends Node
+	
+func apertouBotao(botao):
+	if botao == "leste":
+		return Input.is_action_pressed("leste") and not Input.is_action_pressed('oeste')
+	
+	if botao == "oeste":
+		return Input.is_action_pressed("oeste") and not Input.is_action_pressed('leste')
+	
+	if botao == "sul":
+		return Input.is_action_pressed("sul") and not Input.is_action_pressed('norte')
+	
+	if botao == "norte":
+		return Input.is_action_pressed("norte") and not Input.is_action_pressed('sul')
--- scripts/jogador/Jogador.gd
+++ scripts/jogador/Jogador.gd
@@ -21,12 +21,13 @@ func _ready():
 	mudarAnimacao("paradoSul")
 	
 func rodar(atrito):
+	
 	#fisica
-	if apertouBotao("sul"):
+	if $Input.apertouBotao("sul"):
 		sentido = "sul"
 		# clamp(valor, limite inferior, limite superior)
 		direcao.y = clamp(direcao.y + (vetorSul.y * atrito), vetorNorte.y * velocidade, vetorSul.y * velocidade)
-	elif apertouBotao("norte"):
+	elif $Input.apertouBotao("norte"):
 		sentido = "norte"
 		direcao.y = clamp(direcao.y + (vetorNorte.y * atrito), vetorNorte.y * velocidade, vetorSul.y * velocidade)
 	else:
@@ -35,10 +36,10 @@ func rodar(atrito):
 		elif direcao.y < 0:
 			direcao.y = clamp(direcao.y + (vetorSul.y * atrito), direcao.y, 0)
 		
-	if apertouBotao("oeste"):
+	if $Input.apertouBotao("oeste"):
 		sentido = "horizontal"
 		direcao.x = clamp(direcao.x + (vetorOeste.x * atrito), vetorOeste.x * velocidade, vetorLeste.x * velocidade)
-	elif apertouBotao("leste"):
+	elif $Input.apertouBotao("leste"):
 		sentido = "horizontal"
 		direcao.x = clamp(direcao.x + (vetorLeste.x * atrito), vetorOeste.x * velocidade, vetorLeste.x * velocidade)
 	else:
@@ -50,25 +51,25 @@ func rodar(atrito):
 	mover()
 	
 	#animacao
-	if apertouBotao("oeste"):
+	if $Input.apertouBotao("oeste"):
 		inverterEixoHorizontalSprite(true)
-	elif apertouBotao("leste"):
+	elif $Input.apertouBotao("leste"):
 		inverterEixoHorizontalSprite(false)
 	
-	if apertouBotao("sul"):
-		if apertouBotao("oeste") || apertouBotao("leste"):
+	if $Input.apertouBotao("sul"):
+		if $Input.apertouBotao("oeste") || $Input.apertouBotao("leste"):
 			mudarAnimacao("andandoDiagonalSul")
 		else:
 			mudarAnimacao("andandoSul")
-	elif apertouBotao("norte"):
-		if apertouBotao("oeste") || apertouBotao("leste"):
+	elif $Input.apertouBotao("norte"):
+		if $Input.apertouBotao("oeste") || $Input.apertouBotao("leste"):
 			mudarAnimacao("andandoDiagonalNorte")
 		else:
 			mudarAnimacao("andandoNorte")
 	else:
-		if apertouBotao("oeste"):
+		if $Input.apertouBotao("oeste"):
 			mudarAnimacao("andandoHorizontal")
-		elif apertouBotao("leste"):
+		elif $Input.apertouBotao("leste"):
 			mudarAnimacao("andandoHorizontal")
 
 	if nenhumBotaoDirecaoApertado():
@@ -91,21 +92,10 @@ func estaParado():
 	return direcao == Vector2(0,0)
 
 func nenhumBotaoDirecaoApertado():
-	return not (apertouBotao("norte") || apertouBotao("sul") || apertouBotao("leste") || apertouBotao("oeste"))
+	return not ($Input.apertouBotao("norte") || $Input.apertouBotao("sul") || $Input.apertouBotao("leste") || $Input.apertouBotao("oeste"))
 
 func mudarAnimacao(animacao):
 	$AnimationPlayer.play(animacao)
-
-func apertouBotao(botao):
-	if botao == "sul":
-		#impedir que o usuario aperte dois botoes que são exclusivos
-		return Input.is_action_pressed("sul") and not Input.is_action_pressed("norte")
-	elif botao == "norte":
-		return Input.is_action_pressed("norte") and not Input.is_action_pressed("sul")
-	elif botao == "oeste":
-		return Input.is_action_pressed("oeste") and not Input.is_action_pressed("leste")
-	elif botao == "leste":
-		return Input.is_action_pressed("leste") and not Input.is_action_pressed("oeste")
 
 func inverterEixoHorizontalSprite(b):
 	$Sprite.flip_h = b
