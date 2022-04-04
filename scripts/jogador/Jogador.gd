cc881cc9f5b9ba92e726d77acf79d948b959883d
Guilherme <gui.crb0@gmail.com>
2022-04-04 10:08:52 -0300

    criando vetores isometricos e mudando nome dos sentidos

--- scripts/jogador/Jogador.gd
+++ scripts/jogador/Jogador.gd
@@ -1,75 +1,82 @@
 extends KinematicBody2D
 
-# vetor de direcao
-var direcao = Vector2(0,0)
 
 # velocidade do boneco, export para podermos modificar enquanto jogamos caso queiramos
 export var velocidade = 75
 
+# fisica
+var vetorIsometrico = Vector2(1, 0.5)
+var vetorSul = Vector2(0, vetorIsometrico.y)
+var vetorLeste = Vector2(vetorIsometrico.x, 0)
+var vetorNorte = Vector2(0, -vetorIsometrico.y)
+var vetorOeste = Vector2(-vetorIsometrico.x,0)
+
+# vetor de direcao
+var direcao = Vector2(0,0)
+
 # para sabermos qual sentido ele esta olhando quando esta parado
-var sentido = "baixo"
+var sentido = "sul"
 
 # metodo que roda quando inicia essa cena
 func _ready():
-	mudarAnimacao("paradoBaixo")
+	mudarAnimacao("paradoSul")
 	
 func rodar():
 	
 	#fisica
-	if apertouBotao("baixo"):
-		sentido = "baixo"
-		direcao.y = velocidade/2
-	elif apertouBotao("cima"):
-		sentido = "cima"
-		direcao.y = -velocidade/2
+	if apertouBotao("sul"):
+		sentido = "sul"
+		direcao.y = vetorSul.y * velocidade
+	elif apertouBotao("norte"):
+		sentido = "norte"
+		direcao.y = vetorNorte.y * velocidade
 	else:
 		direcao.y = 0
 		
-	if apertouBotao("esquerda"):
+	if apertouBotao("oeste"):
 		sentido = "horizontal"
-		direcao.x = -velocidade
-	elif apertouBotao("direita"):
+		direcao.x = vetorOeste.x * velocidade
+	elif apertouBotao("leste"):
 		sentido = "horizontal"
-		direcao.x = velocidade
+		direcao.x = vetorLeste.x * velocidade
 	else:
 		direcao.x = 0
 		
 	mover()
 	
 	#animacao
+	if apertouBotao("oeste"):
+		inverterEixoHorizontalSprite(true)
+	elif apertouBotao("leste"):
+		inverterEixoHorizontalSprite(false)
 	
-	if apertouBotao("esquerda"):
-		inverterSentidoSprite(true)
-	elif apertouBotao("direita"):
-		inverterSentidoSprite(false)
-	
-	if apertouBotao("baixo"):
-		if apertouBotao("esquerda") || apertouBotao("direita"):
-			mudarAnimacao("andandoDiagonalBaixo")
+	if apertouBotao("sul"):
+		if apertouBotao("oeste") || apertouBotao("leste"):
+			mudarAnimacao("andandoDiagonalSul")
 		else:
-			mudarAnimacao("andandoBaixo")
-	elif apertouBotao("cima"):
-		if apertouBotao("esquerda") || apertouBotao("direita"):
-			mudarAnimacao("andandoDiagonalCima")
+			mudarAnimacao("andandoSul")
+	elif apertouBotao("norte"):
+		if apertouBotao("oeste") || apertouBotao("leste"):
+			mudarAnimacao("andandoDiagonalNorte")
 		else:
-			mudarAnimacao("andandoCima")
+			mudarAnimacao("andandoNorte")
 	else:
-		if apertouBotao("esquerda"):
+		if apertouBotao("oeste"):
 			mudarAnimacao("andandoHorizontal")
-		elif apertouBotao("direita"):
+		elif apertouBotao("leste"):
 			mudarAnimacao("andandoHorizontal")
 
 	if estaParado():
-		if sentido == "baixo":
-			mudarAnimacao("paradoBaixo")
-		elif sentido == "diagonalBaixo":
-			mudarAnimacao("paradoDiagonalBaixo")
+		if sentido == "sul":
+			mudarAnimacao("paradoSul")
+		elif sentido == "diagonalSul":
+			mudarAnimacao("paradoDiagonalSul")
 		elif sentido == "horizontal":
 			mudarAnimacao("paradoHorizontal")
-		elif sentido == "diagonalCima":
-			mudarAnimacao("paradoDiagonalCima")
-		elif sentido == "cima":
-			mudarAnimacao("paradoCima")
+		elif sentido == "diagonalNorte":
+			mudarAnimacao("paradoDiagonalNorte")
+		elif sentido == "norte":
+			mudarAnimacao("paradoNorte")
 			
 func mover():
 	#metodo que precisa rodar para ele se mover a cada frame
@@ -82,15 +89,15 @@ func mudarAnimacao(animacao):
 	$AnimationPlayer.play(animacao)
 
 func apertouBotao(botao):
-	if botao == "baixo":
+	if botao == "sul":
 		#impedir que o usuario aperte dois botoes que são exclusivos
-		return Input.is_action_pressed("baixo") and not Input.is_action_pressed("cima")
-	elif botao == "cima":
-		return Input.is_action_pressed("cima") and not Input.is_action_pressed("baixo")
-	elif botao == "esquerda":
-		return Input.is_action_pressed("esquerda") and not Input.is_action_pressed("direita")
-	elif botao == "direita":
-		return Input.is_action_pressed("direita") and not Input.is_action_pressed("esquerda")
+		return Input.is_action_pressed("sul") and not Input.is_action_pressed("norte")
+	elif botao == "norte":
+		return Input.is_action_pressed("norte") and not Input.is_action_pressed("sul")
+	elif botao == "oeste":
+		return Input.is_action_pressed("oeste") and not Input.is_action_pressed("leste")
+	elif botao == "leste":
+		return Input.is_action_pressed("leste") and not Input.is_action_pressed("oeste")
 
-func inverterSentidoSprite(b):
+func inverterEixoHorizontalSprite(b):
 	$Sprite.flip_h = b
