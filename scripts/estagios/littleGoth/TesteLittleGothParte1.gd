4df6a2f9653427ace4365ebdc95e0e2b142a7702
Guilherme <gui.crb0@gmail.com>
2022-04-09 10:32:08 -0300

    little goth melodia

--- scripts/estagios/littleGoth/TesteLittleGothParte1.gd
+++ scripts/estagios/littleGoth/TesteLittleGothParte1.gd
@@ -3,4 +3,6 @@ extends "res://scripts/estagios/EstagioTemplate.gd"
 signal mudarEstagio
 
 func _on_MudarEstagio_body_entered(_body):
-	$Gatilhos/Audio.queue_free()
+	$Gatilhos/Audio/Parte1.queue_free()
+	$Gatilhos/Audio/Parte2.global_position = Vector2(0,0)
+	$Gatilhos/Estagio/MudarEstagio.queue_free()
