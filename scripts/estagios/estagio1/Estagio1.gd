6f3ecf115b8ccaeef4e5a677d6ffb1eaf4d377bc
Guilherme <gui.crb0@gmail.com>
2022-05-05 15:52:36 -0300

    marcadores audio se movendo

--- scripts/estagios/estagio1/Estagio1.gd
+++ scripts/estagios/estagio1/Estagio1.gd
@@ -1,5 +1,10 @@
 extends "res://scripts/estagios/RodarEstagio.gd"
 
+var indexIntrumento = 0
+
 func respawnar(body):
 	body.respawnar(Vector2(0,-36), 1)
 
+func _on_AuxiliarAudio350BPM_finished():
+	$Audios/AuxiliarAudio350BPM.play()
+	$GUI.moverMarcadoresAuxilio()
---
+++ scripts/estagios/estagio1/GUI.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+var marcador1Pos
+var marcador2Pos
+var marcador3Pos
+var marcador4Pos
+
+var posMarcador = 0
+
+var posicoesSoma = [
+	Vector2(0,0),
+	Vector2(50,0),
+	Vector2(0,50),
+	Vector2(50,50)
+	]
+
+func _ready():
+	marcador1Pos = $Control/Player1/PlayerMarcador1/Marcador.rect_global_position
+	marcador2Pos = $Control/Player2/PlayerMarcador2/Marcador.rect_global_position
+	marcador3Pos = $Control/Player3/PlayerMarcador3/Marcador.rect_global_position
+	marcador4Pos = $Control/Player4/PlayerMarcador4/Marcador.rect_global_position
+
+func moverMarcadoresAuxilio():
+	$Control/Player1/PlayerMarcador1/Marcador.rect_global_position = marcador1Pos + posicoesSoma[posMarcador]
+	$Control/Player2/PlayerMarcador2/Marcador.rect_global_position = marcador2Pos + posicoesSoma[posMarcador]
+	$Control/Player3/PlayerMarcador3/Marcador.rect_global_position = marcador3Pos + posicoesSoma[posMarcador]
+	$Control/Player4/PlayerMarcador4/Marcador.rect_global_position = marcador4Pos + posicoesSoma[posMarcador]
+	posMarcador = (posMarcador + 1)%4
